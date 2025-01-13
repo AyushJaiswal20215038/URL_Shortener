ba4a8c1802f9f968c5fcc2a7dbf4b329429fc211
AyushJaiswal20215038 <ayushjaiswal20215038@gmail.com>
2025-01-13 18:53:05 +0530

    has done some changes in navbar

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  let token = sessionStorage.getItem("token") || "";
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <>
       <nav className="navbar">
         <div className="logo" onClick={() => navigate("/")}>
           URLShortner
         </div>
-        <button className="login-btn" onClick={() => navigate("/login")}>
-          Login
-        </button>
+        {token.length === 0 ? (
+          <button className="login-btn" onClick={() => navigate("/login")}>
+            Login
+          </button>
+        ) : (
+          <button className="login-btn" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
       </nav>
     </>
   );
