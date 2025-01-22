344936421337d7a19fdd817f0d40696c240a4cc5
AyushJaiswal20215038 <ayushjaiswal20215038@gmail.com>
2025-01-22 05:54:35 +0530

    hosting error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import navbar from "./Components/Navbar";
+// import navbar from "./Components/Navbar";
 import FunctionalityPage from "./screen/Funtionality";
 import HomePage from "./screen/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
